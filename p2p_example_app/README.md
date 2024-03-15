b24b4cf138747f65fd8dc71c31de6e1620439b92
Amber Jain <jainanuamber@gmail.com>
2024-03-15 17:32:42 +0530

    initial commit

---
+++ p2p_example_app/README.md
@@ -0,0 +1,3 @@
+# p2p_example_app
+
+A new Flutter project.
---
+++ p2p_example_app/android/app/src/main/kotlin/com/example/p2p_example_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.p2p_example_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
